bbaa51bf131d7bad0472c8caac7f8635c6abc162
Joe <josephagrane@gmail.com>
2023-06-08 10:02:05 +0100

    added documentation for new functions

--- eufs_tracks/src/eufs_tracks/HeadlessTrackGenerator.py
+++ eufs_tracks/src/eufs_tracks/HeadlessTrackGenerator.py
@@ -16,8 +16,7 @@
 #TODO: parameterize the number of tracks to generate
 from os import path
 from rclpy.node import Node
-import rclpy
-import random
+import rclpy, random, time
 from .TrackGenerator import TrackGenerator as Generator
 from .TrackGenerator import GeneratorContext
 from .ConversionTools import ConversionTools as Converter
@@ -25,14 +24,24 @@ from ament_index_python import get_package_share_directory
 
 class HeadlessTrackGenerator(Node):
     def __init__(self):
+        """
+        This node is responsible for interfacing with the
+        EUFS track generator and generating random tracks.
+        """
         super().__init__("headless_track_generator")
-        random.seed(1)
         self.get_logger().info("Starting headless track generator.")
-        self.declare_ros_parameters()
-        self._generator_values = self.read_ros_parameters()
-        self.generate_random_track("joe3012_random_track_test", True)
+        self._declare_ros_parameters()
+        self._generator_values, self._node_params = self._read_ros_parameters()
+        self._initialise_rng(self._node_params["use_custom_seed"], self._node_params["custom_seed"])
 
-    def declare_ros_parameters(self):
+    def _declare_ros_parameters(self):
+        """
+        This function declares all of the parameters that it expects
+        to receive from ROS.
+        """
+        self.declare_parameter("use_custom_seed", False)
+        self.declare_parameter("custom_seed", 0)
+
         self.declare_parameter("component_data_straight", 1.0)
         self.declare_parameter("component_data_constant_turn", 0.7)
         self.declare_parameter("component_data_hairpin_turn", 0.3)
@@ -48,7 +57,13 @@ class HeadlessTrackGenerator(Node):
         self.declare_parameter("generator_values_lax_generation", False)
         self.declare_parameter("generator_values_track_width", 3.5)
 
-    def read_ros_parameters(self):
+    def _read_ros_parameters(self):
+        """
+        Reads expected ROS parameters.
+
+        :returns: Tuple (2,) in which the first element contains all parameters to be consumed
+        by the track generator and the second element contains any other parameters.
+        """
         component_data = {
             "STRAIGHT" : self.get_parameter("component_data_straight").value,
             "CONSTANT_TURN": self.get_parameter("component_data_constant_turn").value,
@@ -67,9 +82,36 @@ class HeadlessTrackGenerator(Node):
             "TRACK_WIDTH": self.get_parameter("generator_values_track_width").value,
             "COMPONENTS": component_data
             }
-        return generator_values
+        other_params = {
+            "use_custom_seed" : self.get_parameter("use_custom_seed").value,
+            "custom_seed" : self.get_parameter("custom_seed").value
+        }
+        return generator_values, other_params
 
-    def generate_random_track(self, filename, save_image=False):
+    def _initialise_rng(self, use_custom_seed: bool, custom_seed = 0):
+        """
+        Seed the random number generator either with system time or
+        with a custom seed.
+        
+        :param use_custom_seed: Whether or not to use the provided custom seed.
+        :param custom_seed: The custom seed to use.
+        """
+        if use_custom_seed == True:
+            random.seed(custom_seed)
+            self._rng_seed = custom_seed
+        else:
+            t = int(time.time())
+            random.seed(t)
+            self._rng_seed = t
+ 
+    def _generate_random_track(self, filename: str, save_image=False):
+        """
+        This function interfaces with the EUFS random track generator
+        to generate a random track and the associated files.
+        
+        :param filename: The name of the output files to be generated, minus the extension.
+        :param save_image: Whether or not the image file should be saved.
+        """
         tracks_folder = get_package_share_directory("eufs_tracks")
         images_folder = path.join(tracks_folder, "image")
         generator_values = self._generator_values

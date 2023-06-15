d65c1094acdf1793c49e2d0df16d5370a8816f4a
Joe <josephagrane@gmail.com>
2023-06-15 09:32:02 +0100

    fixed seeding for numpy by adding int cast

--- eufs_tracks/src/eufs_tracks/HeadlessTrackGenerator.py
+++ eufs_tracks/src/eufs_tracks/HeadlessTrackGenerator.py
@@ -124,7 +124,7 @@ class HeadlessTrackGenerator(Node):
         custom = self._node_params["seed"]
         seed = custom if use_custom else time.time()
         random.seed(seed)
-        numpy.random.seed(seed)
+        numpy.random.seed(int(seed))
         if use_custom: self.get_logger().info(f"Using a custom seed: {custom}")
         return seed
  
